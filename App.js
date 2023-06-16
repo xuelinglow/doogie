fbf3eeafca938cea4f6f0784b75b3c3f6eb4564b
unknown <xuelinglow09@gmail.com>
2023-06-17 02:02:58 +0800

    test

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from './screens/mainScreen';
